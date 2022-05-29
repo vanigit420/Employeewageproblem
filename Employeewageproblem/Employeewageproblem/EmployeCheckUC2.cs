3147129a02073e7c5dcddea308540bdd333eb99d
shivani korwan <shivanikorwan63232@gmail.com>
2022-05-29 13:15:09 +0530

    checking salary

--- Employeewageproblem/Employeewageproblem/EmployeCheckUC2.cs
+++ Employeewageproblem/Employeewageproblem/EmployeCheckUC2.cs
@@ -4,24 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageprogram
+namespace EmployeeWageProgram
 {
-    internal class EmployeeCheck
+    internal class EmployeeWage
     {
-        public static void EmployeePresentOrNot()
+        public static void EmployeeRatePerHr()
         {
+
             int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hr = 20;
+            int empwage = 0;
+            int emphrs = 0;
             Random random = new Random();
             int empCheck = random.Next(2);
             if (empCheck == Is_Full_Time)
             {
                 Console.WriteLine("Employee is Present");
+                emphrs = 8;
+
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                emphrs = 0;
             }
+            empwage = emphrs * Emp_Rate_Per_Hr;
+            Console.WriteLine("Emp Wage :" + empwage);
+            Console.ReadKey();
         }
     }
 }
-
